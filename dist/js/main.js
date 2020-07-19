bb4b696d969c04082b7d29b2acf4426c7a4195f0
Angel Figueroa <afigueroa742@gmail.com>
2020-07-18 23:47:07 -0400

    Fix mobile design hamburger navigation menu

---
+++ dist/js/main.js
@@ -0,0 +1,33 @@
+// grab DOM from elements
+const menuButton = document.querySelector('.menu-button');
+const navigation = document.querySelector('.navigation');
+const navigationUl = document.querySelector('.navigation__ul');
+const navigationItems = document.querySelectorAll('.navigation__item');
+
+
+// set initial state of menu 
+let showMenu = false;
+
+menuButton.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuButton.classList.add('close');
+    navigation.classList.add('show');
+    navigationUl.classList.add('show');
+    navigationItems.forEach(item => item.classList.add('show'));
+
+    // set menu state 
+    showMenu = true;
+
+  } else {
+
+    menuButton.classList.remove('close');
+    navigation.classList.remove('show');
+    navigationUl.classList.remove('show');
+    navigationItems.forEach(item => item.classList.remove('show'));
+
+    // set menu state
+    showMenu = false;
+  }
+}
